48c7b103fc5bee3b0ce95cca609c2ef06e948ee2
Harish Agrawal <haragraw5@publicisgroupe.net>
2022-07-19 20:42:48 +0530

    ci/cd pipeline test

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -20,11 +20,8 @@ const devConfig = {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js',
       },
       shared: packageJson.dependencies
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
+    })
+  ]
 };
 
 module.exports = merge(commonConfig, devConfig);

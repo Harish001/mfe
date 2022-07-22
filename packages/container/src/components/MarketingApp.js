8eeb15224c8b242da93da56588a5a7cd94809231
Harish Agrawal <haragraw5@publicisgroupe.net>
2022-07-22 16:36:57 +0530

    routing setup of container and marketing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,23 @@
 import { mount } from 'marketing/MarketingApp';
 import React, {useRef, useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
 
 export default () => {
     const ref = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        mount(ref.current)
-    })
+        const {onParentNavigate} = mount(ref.current, {
+            onNavigate: (location) => {
+                const {pathname} = history.location;
+                if (location && pathname !== location.pathname) {
+                    history.push(location.pathname);
+                }
+            }
+        });
+
+        history.listen(onParentNavigate);
+    }, [])
 
     return <div ref={ref}></div>
 
